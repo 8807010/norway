d60314a5534c81340cf286aefaacd824122f809b
Artiom_Sokolov <sokolova-net@mail.ru>
2023-05-01 19:53:35 +0300

    add preview image

--- src/js/components/disabled-btn.js
+++ src/js/components/disabled-btn.js
@@ -1,11 +1,11 @@
 const button = document.getElementById('btn');
-// button.setAttribute('disabled', '');
+button.setAttribute('disabled', '');
 
-// if (window.innerWidth >= 1140) {
-//   button.setAttribute('disabled', '');
-// } else {
-//   button.removeAttribute('disabled');
-// };
+if (window.innerWidth >= 1140) {
+  button.setAttribute('disabled', '');
+} else {
+  button.removeAttribute('disabled');
+};
 
 window.onresize = function () {
   if (window.innerWidth >= 1140) {
